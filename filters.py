be4aeeaec5de190607a99db2c86abd542b4d21dc
Davor Padovan <davor.padovan@gmail.com>
2015-04-21 17:11:48 +0200

    Cleanup and styling.

--- filters.py
+++ filters.py
@@ -1,16 +1,5 @@
-import re
-from jinja2 import evalcontextfilter, Markup, escape
+from jinja2 import evalcontextfilter
 from libs import markdown
-
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
-@evalcontextfilter
-def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
-                          for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
 
 @evalcontextfilter
 def markitdown(eval_ctx, value):
