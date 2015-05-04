58c923e1d53637a65c3c47b9da7d9ec0e9521859
Davor Padovan <davor.padovan@gmail.com>
2015-05-04 08:54:01 +0200

    Version bump.

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,24 +1,25 @@
 from google.appengine.api import users
-from models.topic import Topic
 from settings import ADMINS
 
 
 def user_required(handler):
     def check_login(self, *args, **kwargs):
         user = users.get_current_user()
+
         if user:
             return handler(self, *args, **kwargs)
         else:
-            return self.redirect("/")
+            return self.redirect(users.create_login_url('/'))
 
     return check_login
 
 def admin_required(handler):
     def check_login(self, *args, **kwargs):
         username = users.get_current_user().nickname()
+
         if username in ADMINS:
             return handler(self, *args, **kwargs)
         else:
-            return self.redirect("/")
+            return self.redirect(users.create_login_url('/'))
 
     return check_login
